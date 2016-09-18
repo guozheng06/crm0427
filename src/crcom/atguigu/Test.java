95ea51afed6f5ab5b9f56914050929f622221675
guozheng06 <guozheng0628@163.com>
2016-09-18 21:12:34 +0800

    ffffffff

--- src/crcom/atguigu/Test.java
+++ src/crcom/atguigu/Test.java
@@ -4,5 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("0427-----00");
 		System.out.println("0427-----01");
+		System.out.println("0427-----02");
+		System.out.println("0427-----03");
 	}
 }
